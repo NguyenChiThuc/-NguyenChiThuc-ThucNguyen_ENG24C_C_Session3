db42538abcf1a8ee5775acc87904b5197b5e57aa
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-21 23:37:51 +0700

    Upload bai tap SS3 - 1

---
+++ SS3 - B1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+	//Khai bao do dai cua 1 sau
+	char Name[20];
+	
+	//Moi nhap ten va thuc hien yeu cau sau do in ra
+	
+
+	printf("Nhap ten cua ban");
+	scanf("%s", &Name);
+	printf("Xin chao %s", Name);
+	
+	return 0;
+}
